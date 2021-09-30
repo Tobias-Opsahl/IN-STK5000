dd58ed615ed13a3bdfb14a7109dc80712a419f89
Tobias Opsahl <tobiasop@gmail.com>
2021-09-30 14:47:49 +0200

    add init function for reading

--- Project1/project1.py
+++ Project1/project1.py
@@ -2,42 +2,63 @@ import numpy as np
 import pandas as pd 
 from IPython import embed
 
-
-actions = pd.read_csv("treatment_actions.csv")
-features = pd.read_csv("treatment_features.csv")
-outcomes = pd.read_csv("treatment_outcomes.csv")
-
-
 def print_df(data):
+    """
+    Print summary of the whole dataframe
+    """
     with pd.option_context('display.max_rows', None,
                        'display.max_columns', None,
                        'display.precision', 3,
                        ):
         print(data.describe())
 
-# embed()
-# print_df()
 
-post_treatment = ["Past-Covid", "Covid+", "No-Taste/Smell", "Fever", "Headache", 
-                  "Pneumonia", "Stomach", "Myocarditis", "Blood-Clots", "Death"]
-
-def init_symptoms():
-    observation_features = pd.read_csv("observation_features.csv")
-    Symptoms =  ["Covid-Recovered", "Age", "Gender", "Income", "Genome", "Comorbidities", "Vaccination status"]
-    symptom_names = []
-    symptom_names += ["Covid-Recovered" + str(i) for i in range(1, 11)]
-    symptom_names += ["Age", "Gender", "Income"]
-    symptom_names += ["Genome" + str(i) for i in range(1, 129)]
-    symptom_names += ["Comorbidities" + str(i) for i in range(1, 7)]
-    symptom_names += ["Vaccination status" + str(i) for i in range(1, 4)]
-    observation_features.columns = symptom_names
-    return observation_features
+def init_features(data):
+    """
+    Initialize names for observation features and treatment features
     
-print(actions.columns)
-# def init_actions
-# print_df(observation_features)
-# 10, 1, 1, 1, 128, 6, 3
-             
-# Symptoms (10 bits): Covid-Recovered, Covid-Positive, No-Taste/Smell, Fever, Headache, Pneumonia, Stomach, Myocarditis, Blood-Clots, Death
-# Comorbidities (6 bits): Asthma, Obesity, Smoking, Diabetes, Heart disease, Hypertension
-# Vaccination status (3 bits): 0 for unvaccinated, 1 for receiving a specific vaccine for each bit
+    Symptoms (10 bits): Covid-Recovered, Covid-Positive, No-Taste/Smell, 
+        Fever, Headache, Pneumonia, Stomach, Myocarditis, Blood-Clots, Death
+    Age (integer)
+    Gender (binary)
+    Income (floating)
+    Genome (128 bits)
+    Comorbidities (6 bits): Asthma, Obesity, Smoking, Diabetes, Heart disease, Hypertension
+    Vaccination status (3 bits): 0 for unvaccinated, 1 for receiving a specific vaccine for each bit
+    """
+    features_data = pd.read_csv(data)
+    # features =  ["Covid-Recovered", "Age", "Gender", "Income", "Genome", "Comorbidities", "Vaccination status"]
+    features = []
+    features += ["Covid-Recovered" + str(i) for i in range(1, 11)]
+    features += ["Age", "Gender", "Income"]
+    features += ["Genome" + str(i) for i in range(1, 129)]
+    features += ["Comorbidities" + str(i) for i in range(1, 7)]
+    features += ["Vaccination status" + str(i) for i in range(1, 4)]
+    features_data.columns = features
+    return features_data
+    
+
+def init_actions():
+    actions = pd.read_csv("treatment_actions.csv")
+    actions.columns = ["Treatment1", "Treatment2"]
+    return actions 
+    
+def init_outcomes():
+    """
+    Initialize outcome data
+    
+    Post-Treatment Symptoms (10 bits): Past-Covid (Ignore), Covid+ (Ignore), 
+    No-Taste/Smell, Fever, Headache, Pneumonia, Stomach, Myocarditis, 
+    Blood-Clots, Death
+    """
+    outcomes = pd.read_csv("treatment_outcomes.csv")
+    outcome_names = ["Past-Covid", "Covid+", "No-Taste/Smell", "Fever", "Headache", 
+                      "Pneumonia", "Stomach", "Myocarditis", "Blood-Clots", "Death"]
+    outcomes.columns = outcome_names
+    return outcomes
+
+actions = init_actions()
+outcomes = init_outcomes()
+observation_features = init_features("observation_features.csv")
+treatment_features = init_features("treatment_features.csv")
+embed()
