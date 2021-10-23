f78cb1766b60f8192458f125b23ca5f09def1dee
Tobias Opsahl <tobiasop@gmail.com>
2021-10-23 12:17:31 +0200

    make find_correlation

--- Project1/find_correlation.py
+++ Project1/find_correlation.py
@@ -1,106 +1,11 @@
+# File trying to find correlation in the dataset, 1a)
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.datasets import load_digits
-from sklearn.feature_selection import SelectPercentile, chi2
 from IPython import embed
 
 np.random.seed(57)
-
-# plt.show()
-
-def correlated_normal_data(n, c, num_samples):
-    """
-    n: Dimension of covariance matrix. 
-    c: Number of correlated columns
-    n-c: Number of non-correlated columns. 
-    """
-    cov_matrix = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                cov_matrix[i, j] = 1 + 1e-10 # Should be 1, but numpy warnign
-            # else: 
-            #     cov_matrix[i, j] = 1e-10
-    for i in range(c):
-        for j in range(i):
-            if i != j: 
-                correlation = np.random.uniform(0.5, 1)
-                cov_matrix[i, j] = correlation
-                cov_matrix[j, i] = correlation
-    # mean = np.random.uniform(0, 1, n)
-    mean = np.ones(n)
-    y = np.random.multivariate_normal(mean, cov_matrix, size=num_samples)
-    return mean, cov_matrix, y
-    
-def correlated_categorical_data(num_col, c, num_samples):
-    prob = 0.8
-    response = np.random.randint(2, size=num_samples)
-    data = np.zeros((num_samples, num_col))
-    for i in range(num_samples):
-        for j in range(num_col):
-            if j < c: # Correlated
-                coin_flip = np.random.uniform()
-                if coin_flip < prob: 
-                    data[i, j] = response[i]
-                else: 
-                    data[i, j] = np.random.randint(2)
-            elif j >= c: # Not correlated
-                data[i, j] = np.random.randint(2)
-    data = pd.DataFrame(data)
-    return response, data
-            
-def correlated_categorical_data2(num_col, c, num_samples):
-    """
-    Create correlated data. The "num_col" explanatory vairables are totally 
-    random, between 0 and 1. Then the response is 1 if the first c columns
-    have a sum bigger than 0.5, if not 0. 
-    """
-    prob = 1
-    response = np.random.randint(2, size=num_samples)
-    data = np.zeros((num_samples, num_col))
-    for i in range(num_col):
-        data[:, i] = np.random.randint(2, size=num_samples)
-    for i in range(num_samples):
-        if sum(data[i, :c])/c > 0.5:
-            coin_flip = np.random.uniform()
-            if coin_flip < prob: 
-                response[i] = 1
-            else: 
-                response[i] = 0
-                # response[i] = np.random.randint(2)
-        else:
-            # response[i] = 0   
-            response[i] = 0
-            # response[i] = np.random.randint(2)
-        
-    data = pd.DataFrame(data)
-    return response, data  
-
-def plot_3d(mean, y):
-    # Plot various projections of the samples.
-    plt.subplot(2,2,1)
-    plt.plot(y[:,0], y[:,1], 'b.')
-    plt.plot(mean[0], mean[1], 'ro')
-    plt.ylabel('y[1]')
-    plt.axis('equal')
-    plt.grid(True)
-    
-    plt.subplot(2,2,3)
-    plt.plot(y[:,0], y[:,2], 'b.')
-    plt.plot(mean[0], mean[2], 'ro')
-    plt.xlabel('y[0]')
-    plt.ylabel('y[2]')
-    plt.axis('equal')
-    plt.grid(True)
-    
-    plt.subplot(2,2,4)
-    plt.plot(y[:,1], y[:,2], 'b.')
-    plt.plot(mean[1], mean[2], 'ro')
-    plt.xlabel('y[1]')
-    plt.axis('equal')
-    plt.grid(True)
-    plt.show()
 
 def create_correlated_data(num_col, num_cor, num_row, prob=0.9):
     """
@@ -179,25 +84,8 @@ def correlation_select(data, response, correlation_threshold=0.01):
         if cor > correlation_threshold:
             selected_columns.append(i)
     return selected_columns
-    
 
 data, response = create_correlated_data(128, 10, 100000)
 cor_data = np.c_[response, data] # Merge
 # print(cor_data)
 print(correlation_select(data, response))
-# embed()
-# response, data = correlated_categorical_data2(128, 3, 100)
-# # print(data)
-# # print(response)    
-# print(np.matrix(data)[1, :10])
-# 
-# x_new = SelectPercentile(chi2, percentile=5).fit_transform(data, response)
-# print(x_new)
-# print(data.shape)
-# print(x_new.shape)
-# mean, cov_matrix, y = correlated_data(5, 3, 1000)
-# print(y.shape)
-# print(y)
-# plot_3d(mean, y)
-
-            
