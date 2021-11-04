b76a07603c726920b65793674f85a6b277f3b441
Tobias Opsahl <tobiasop@gmail.com>
2021-11-04 12:17:09 +0100

    try to make policy

--- project2/policy.py
+++ project2/policy.py
@@ -6,8 +6,10 @@
 ## outcomes: symptoms (including covid-positive)
 
 import numpy as np
+import pandas as pd
 from aux_file import symptom_names
 import simulator
+from IPython import embed
 
 class Policy:
     """ A policy for treatment/vaccination. """
@@ -38,7 +40,12 @@ class Policy:
             self.data.append(features, actions, outcomes)
             self.model.fit(data)
         """
-        pass
+        df1 = add_feature_names(features)
+        df2 = add_action_names(actions)
+        df3 = add_outcome_names(outcomes)
+        df = df1.join(df2.join(df3))
+        
+        
     def get_utility(self, features, action, outcome):
         """ Obtain the empirical utility of the policy on a set of one or more people. 
         If there are t individuals with x features, and the action
@@ -49,9 +56,27 @@ class Policy:
         outcomes (t*|Y| array)
         Returns:
         Empirical utility of the policy on this data.
+        """    
+        # df1 = add_feature_names(features)
+        # df2 = add_action_names(actions)
+        # df3 = add_outcome_names(outcomes)
+        # df = df1.join(df2.join(df3))
+        """Here the utiliy is defined in terms of the outcomes obtained only, ignoring both the treatment and the previous condition.
         """
+        actions = self.get_action(features)
+        utility = 0
+        utility -= 0.2 * sum(outcome[:,symptom_names['Covid-Positive']])
+        utility -= 0.1 * sum(outcome[:,symptom_names['Taste']])
+        utility -= 0.1 * sum(outcome[:,symptom_names['Fever']])
+        utility -= 0.1 * sum(outcome[:,symptom_names['Headache']])
+        utility -= 0.5 * sum(outcome[:,symptom_names['Pneumonia']])
+        utility -= 0.2 * sum(outcome[:,symptom_names['Stomach']])
+        utility -= 0.5 * sum(outcome[:,symptom_names['Myocarditis']])
+        utility -= 1.0 * sum(outcome[:,symptom_names['Blood-Clots']])
+        utility -= 100.0 * sum(outcome[:,symptom_names['Death']])
+        return utility
+        
 
-        return 0
     def get_action(self, features):
         """Get actions for one or more people. 
         Args: 
@@ -68,7 +93,15 @@ class Policy:
            return argmax(u)
         You are expected to create whatever helper functions you need.
         """
-        return 0
+        n_people = features.shape[0]
+        df = add_feature_names(features)
+        actions = np.zeros([n_people, self.n_actions])
+        for t in range(features.shape[0]):
+            if df.iloc[t, symptom_names['Myocarditis']] == 1:
+                actions[t, 0] = 1
+            else:
+                actions[t, 2] = 1
+        return actions
 
 class RandomPolicy(Policy):
     """ This is a purely random policy!"""
@@ -105,10 +138,42 @@ class RandomPolicy(Policy):
             action = np.random.choice(self.action_set)
             if (action >= 0):
                 actions[t,action] = 1
+            # embed()
             
         return actions
 
+def add_feature_names(X):
+    features_data = pd.DataFrame(X)
+    # features =  ["Covid-Recovered", "Age", "Gender", "Income", "Genome", "Comorbidities", "Vaccination status"]
+    features = []
+    # features += ["Symptoms" + str(i) for i in range(1, 11)]
+    features += ["Covid-Recovered", "Covid-Positive", "No-Taste/Smell", "Fever", 
+                 "Headache", "Pneumonia", "Stomach", "Myocarditis", 
+                 "Blood-Clots", "Death"]
+    features += ["Age", "Gender", "Income"]
+    features += ["Genome" + str(i) for i in range(1, 129)]
+    # features += ["Comorbidities" + str(i) for i in range(1, 7)]
+    features += ["Asthma", "Obesity", "Smoking", "Diabetes", 
+                 "Heart disease", "Hypertension"]
+    features += ["Vaccination status" + str(i) for i in range(1, 4)]
+    features_data.columns = features
+    return features_data
+    
+def add_action_names(actions):
+    df = pd.DataFrame(actions)
+    names = ["Action" + str(i) for i in range(1, len(actions.shape[0]) + 1)]
+    df.columns = names
+    return df
+
+def add_outcome_names(outcomes):
+    df = pd.DataFrame(outcomes)
+    df.columns = ["Covid-Recovered", "Covid-Positive", "No-Taste/Smell", "Fever", 
+                  "Headache", "Pneumonia", "Stomach", "Myocarditis", 
+                  "Blood-Clots", "Death"]
+    return df
+    
 if __name__ == "__main__":
+    np.random.seed(57)
     policy = Policy(3, ["treatment1", "treatment2", "treatment3"])
     n_genes = 128
     n_vaccines = 3
@@ -118,17 +183,31 @@ if __name__ == "__main__":
     # Create the underlying population
     print("Generating population")
     population = simulator.Population(n_genes, n_vaccines, n_treatments)
+    vaccine_policy = Policy(n_vaccines, list(range(-1,n_vaccines))) # make sure to add -1 for 'no vaccine'
     X = population.generate(n_population)
-    print("Vaccination")
-    vaccine_policy = RandomPolicy(n_vaccines, list(range(-1,n_vaccines))) # make sure to add -1 for 'no vaccine'
+    A = vaccine_policy.get_action(X)
+    V = population.vaccinate(list(range(n_population)), A)
+    df = add_feature_names(X)
+    embed()
+    # In [6]: A.shape
+    # Out[6]: (1000, 3)
+    # 
+    # In [7]: V.shape
+    # Out[7]: (1000, 10)
+    # 
+    # In [8]: X.shape
+    # Out[8]: (1000, 150)
+    # embed()
     
+    # print(type(X))
     
-    print("With a for loop")
-    # The simplest way to work is to go through every individual in the population
-    for t in range(n_population):
-        a_t = vaccine_policy.get_action(X[t])
-        # Then you can obtain results for everybody
-        y_t = population.vaccinate([t], a_t)
-        # Feed the results back in your policy. This allows you to fit the
-        # statistical model you have.
-        vaccine_policy.observe(X[t], a_t, y_t)
+    # print("Vaccination")
+    # print("With a for loop")
+    # # The simplest way to work is to go through every individual in the population
+    # for t in range(n_population):
+    #     a_t = vaccine_policy.get_action(X[t])
+    #     # Then you can obtain results for everybody
+    #     y_t = population.vaccinate([t], a_t)
+    #     # Feed the results back in your policy. This allows you to fit the
+    #     # statistical model you have.
+    #     vaccine_policy.observe(X[t], a_t, y_t)
